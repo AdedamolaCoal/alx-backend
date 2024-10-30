eae28883ebcf4b0ccb7752893c549ac64ece3f3c
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-30 21:42:35 +0100

    task 2 done

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,8 +5,8 @@ from collections import OrderedDict
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """FIFO caching system"""
+class LIFOCache(BaseCaching):
+    """LIFO caching system"""
 
     def __init__(self):
         """Initiliaze"""
@@ -15,13 +15,15 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item in the cache"""
-        self.cache_data[key] = item
         if key is None or item is None:
             return
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # this is called tuple unpacking _
-            least_used, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {least_used}")
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                # this is called tuple unpacking _
+                least_used, _ = self.cache_data.popitem(last=True)
+                print(f"DISCARD: {least_used}")
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """returns the get value for the key"""
