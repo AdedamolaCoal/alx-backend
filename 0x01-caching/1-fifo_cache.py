3bc047ec425fad401b26a86015baea74e795c239
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-30 20:39:05 +0100

    update

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -19,13 +19,15 @@ class FIFOCache(BaseCaching):
         if key is None or item is None:
             return
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # least_used = min(self.cache_data, key=self.cache_data.get)
             # this is called tuple unpacking _
             least_used, _ = self.cache_data.popitem(last=False)
-            # del self.cache_data[least_used]
             print(f"DISCARD: {least_used}")
 
     def get(self, key):
         if key is None or key not in self.cache_data:
             return
         return self.cache_data.get(key, None)
+
+
+# least_used = min(self.cache_data, key=self.cache_data.get)
+# del self.cache_data[least_used]
